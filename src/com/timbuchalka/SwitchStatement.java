c0b21b4299e8ae88af9a910eb0a0ad6e01436061
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-11 16:03:20 -0400

    Implemented challenge's switch statement

--- src/com/timbuchalka/SwitchStatement.java
+++ src/com/timbuchalka/SwitchStatement.java
@@ -43,6 +43,22 @@ public class SwitchStatement {
         // display a message if any of these are found and then break
         // Add a default which displays a message saying not found
 
+        char charValue = 'G';
+        switch(charValue) {
+            case 'A':
+                System.out.println("A was found");
+                break;
+            case 'B':
+                System.out.println("B was found");
+                break;
+            case 'C': case 'D' : case 'E':
+                System.out.println(charValue + " was found");
+                break;
+
+            default:
+                System.out.println("Could not find A, B, C, D or E");
+                break;
+        }
     }
 
 }
