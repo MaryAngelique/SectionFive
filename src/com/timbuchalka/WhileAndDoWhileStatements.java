fb5b39cad35a416eedb27c778aeb233897d68a58
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-12 17:03:24 -0400

    Implemented challenge instruction

--- src/com/timbuchalka/WhileAndDoWhileStatements.java
+++ src/com/timbuchalka/WhileAndDoWhileStatements.java
@@ -4,27 +4,44 @@ public class WhileAndDoWhileStatements {
 
     public static void main(String[] args) {
 	    int count = 6;
-        while(count != 6) {
-            System.out.println("Count value is " + count);
-            count++;
-        }
+//        while(count != 6) {
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+//
+//        for(int i=6; i!= 6; i++) {
+//            System.out.println("Count value is " + count);
+//        }
 
-        for(int i=6; i!= 6; i++) {
-            System.out.println("Count value is " + count);
-        }
-
-        count = 6;
-        do {
-            System.out.println("Count value was " + count);
-            count++;
-
-            if(count >100) {
-                break;
-            }
-
-        } while(count != 6);
+//        count = 6;
+//        do {
+//            System.out.println("Count value was " + count);
+//            count++;
+//
+//            if(count >100) {
+//                break;
+//            }
+//
+//        } while(count != 6);
+//
+//        int number = 5;
+//        int finishNumber = 20;
+//        while(number <= finishNumber) {
+//            if(!isEvenNumber(number)) {
+//                number++;
+//                continue;
+//            }
+//
+//            System.out.println("Even number " + number);
+//            number++;
+//        }
 
 
     }
+
+    // Create a method called isEvenNumber that takes a parameter of type int
+    // Its purpose is to determine if the argument passed to the method is
+    // an even number or not.
+    // return true if an even number, otherwise return false;
 
 }
