9d55a9a0bea94d8ec5d679623988e756863f113b
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-13 11:35:16 -0400

    Implemented number as a string

---
+++ src/com/timbuchalka/ParsingValuesFromAString.java
@@ -0,0 +1,12 @@
+package com.timbuchalka;
+
+public class ParsingValuesFromAString {
+
+    public static void main(String[] args) {
+        String numberAsString = "2018.125";
+        System.out.println("numberAsString= " + numberAsString);
+
+
+    }
+
+}
