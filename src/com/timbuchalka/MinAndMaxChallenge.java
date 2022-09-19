7a2efe7c45f17b718fe45fa51764e35e6ddbc796
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-19 11:25:10 -0400

    Implemented Scanner value and min and max variables

--- src/com/timbuchalka/MinAndMaxChallenge.java
+++ src/com/timbuchalka/MinAndMaxChallenge.java
@@ -20,31 +20,7 @@ public class MinAndMaxChallenge {
         int min = Integer.MAX_VALUE;
         int max = Integer.MIN_VALUE;
 
-        while(true) {
 
-            System.out.println("Enter number:");
-            boolean isAnInt = scanner.hasNextInt();
-
-            if(isAnInt) {
-
-                int number = scanner.nextInt();
-
-                if(number > max) {
-                    max = number;
-                }
-
-                if(number < min) {
-                    min = number;
-                }
-
-            } else {
-                break;
-            }
-
-            scanner.nextLine(); // handle input
-        }
-
-        System.out.println("min= "+ min + ", max= " + max);
         scanner.close();
 
     }
