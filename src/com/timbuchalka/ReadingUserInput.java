f8011ede84c252948e50a831b7dd55e3fe71227c
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-18 22:33:04 -0400

    Implemented an if statement with the commented code block

--- src/com/timbuchalka/ReadingUserInput.java
+++ src/com/timbuchalka/ReadingUserInput.java
@@ -12,22 +12,24 @@ public class ReadingUserInput {
 
         boolean hasNextInt = scanner.nextInt();
 
-//        int birthYear = scanner.nextInt();
-//        scanner.nextLine();
-//        // handles next line character
-//
-//        System.out.println("Enter your name: ");
-//        String name = scanner.nextLine();
-//        int age = 2022 - birthYear;
-//
-//        if(age >= 0 && age <= 100) {
-//            System.out.println("Your name is " + name + ", and you are " + age + "years old.");
-//
-//        } else {
-//            System.out.println("Invalid birth year");
-//        }
-//
-//        System.out.println("Your name is " + name + ", and you are " + age + "years old");
+        if(hasNextInt) {
+            int birthYear = scanner.nextInt();
+            scanner.nextLine();
+            // handles next line character
+
+            System.out.println("Enter your name: ");
+            String name = scanner.nextLine();
+            int age = 2022 - birthYear;
+
+            if(age >= 0 && age <= 100) {
+                System.out.println("Your name is " + name + ", and you are " + age + "years old.");
+
+            } else {
+                System.out.println("Invalid birth year");
+            }
+
+            System.out.println("Your name is " + name + ", and you are " + age + "years old");
+        }
 
         scanner.close();
 
