bf9cb9a2654fae1692f6fc6d2a4d34d1d2040cbf
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-19 11:24:21 -0400

    Implemented java util Scanner import

--- src/com/timbuchalka/MinAndMaxChallenge.java
+++ src/com/timbuchalka/MinAndMaxChallenge.java
@@ -1,5 +1,7 @@
 package com.timbuchalka;
 
+import java.util.Scanner;
+
 public class MinAndMaxChallenge {
 //  -Read the numbers from the console entered by the user and print the minimum and maximum number the user has entered.
 //  -Before the user enters the number, print the message ÅgEnter number:Åh
@@ -11,4 +13,39 @@ public class MinAndMaxChallenge {
 //            Bonus:
 //            -Create a project with the name MinAndMaxInputChallenge.
 
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        while(true) {
+
+            System.out.println("Enter number:");
+            boolean isAnInt = scanner.hasNextInt();
+
+            if(isAnInt) {
+
+                int number = scanner.nextInt();
+
+                if(number > max) {
+                    max = number;
+                }
+
+                if(number < min) {
+                    min = number;
+                }
+
+            } else {
+                break;
+            }
+
+            scanner.nextLine(); // handle input
+        }
+
+        System.out.println("min= "+ min + ", max= " + max);
+        scanner.close();
+
+    }
 }
