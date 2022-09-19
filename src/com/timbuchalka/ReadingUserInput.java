ac73e15543fabe5e3fc981076bd57948df5a55db
Angelique Abacajan <ma_utsey@yahoo.com>
2022-09-18 22:07:48 -0400

    Implemented a boolean hasNextInt variable and commented out the rest of the code

--- src/com/timbuchalka/ReadingUserInput.java
+++ src/com/timbuchalka/ReadingUserInput.java
@@ -9,22 +9,25 @@ public class ReadingUserInput {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter your birth year: ");
-        int birthYear = scanner.nextInt();
-        scanner.nextLine();
-        // handles next line character
 
-        System.out.println("Enter your name: ");
-        String name = scanner.nextLine();
-        int age = 2022 - birthYear;
+        boolean hasNextInt = scanner.nextInt();
 
-        if(age >= 0 && age <= 100) {
-            System.out.println("Your name is " + name + ", and you are " + age + "years old.");
-
-        } else {
-            System.out.println("Invalid birth year");
-        }
-
-        System.out.println("Your name is " + name + ", and you are " + age + "years old");
+//        int birthYear = scanner.nextInt();
+//        scanner.nextLine();
+//        // handles next line character
+//
+//        System.out.println("Enter your name: ");
+//        String name = scanner.nextLine();
+//        int age = 2022 - birthYear;
+//
+//        if(age >= 0 && age <= 100) {
+//            System.out.println("Your name is " + name + ", and you are " + age + "years old.");
+//
+//        } else {
+//            System.out.println("Invalid birth year");
+//        }
+//
+//        System.out.println("Your name is " + name + ", and you are " + age + "years old");
 
         scanner.close();
 
